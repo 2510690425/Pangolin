958a015fc02ba7f70f2109271055b7d87d0c4e4a
cyx_ubuntu16.04 <2510690425@qq.com>
2025-04-19 03:09:21 +0800

    适配ubuntu16.04

---
+++ build/src/CMakeFiles/pangolin.dir/DependInfo.cmake
@@ -0,0 +1,61 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/cyx/Softwares/SensorsCalibration/lidar2lidar/Pangolin/build/src/fonts.c" "src/CMakeFiles/pangolin.dir/fonts.c.o" "gcc" "src/CMakeFiles/pangolin.dir/fonts.c.o.d"
+  "/home/cyx/Softwares/SensorsCalibration/lidar2lidar/Pangolin/src/console/ConsoleView.cpp" "src/CMakeFiles/pangolin.dir/console/ConsoleView.cpp.o" "gcc" "src/CMakeFiles/pangolin.dir/console/ConsoleView.cpp.o.d"
+  "/home/cyx/Softwares/SensorsCalibration/lidar2lidar/Pangolin/src/display/device/display_x11.cpp" "src/CMakeFiles/pangolin.dir/display/device/display_x11.cpp.o" "gcc" "src/CMakeFiles/pangolin.dir/display/device/display_x11.cpp.o.d"
+  "/home/cyx/Softwares/SensorsCalibration/lidar2lidar/Pangolin/src/display/display.cpp" "src/CMakeFiles/pangolin.dir/display/display.cpp.o" "gcc" "src/CMakeFiles/pangolin.dir/display/display.cpp.o.d"
+  "/home/cyx/Softwares/SensorsCalibration/lidar2lidar/Pangolin/src/display/opengl_render_state.cpp" "src/CMakeFiles/pangolin.dir/display/opengl_render_state.cpp.o" "gcc" "src/CMakeFiles/pangolin.dir/display/opengl_render_state.cpp.o.d"
+  "/home/cyx/Softwares/SensorsCalibration/lidar2lidar/Pangolin/src/display/view.cpp" "src/CMakeFiles/pangolin.dir/display/view.cpp.o" "gcc" "src/CMakeFiles/pangolin.dir/display/view.cpp.o.d"
+  "/home/cyx/Softwares/SensorsCalibration/lidar2lidar/Pangolin/src/display/viewport.cpp" "src/CMakeFiles/pangolin.dir/display/viewport.cpp.o" "gcc" "src/CMakeFiles/pangolin.dir/display/viewport.cpp.o.d"
+  "/home/cyx/Softwares/SensorsCalibration/lidar2lidar/Pangolin/src/display/widgets/widgets.cpp" "src/CMakeFiles/pangolin.dir/display/widgets/widgets.cpp.o" "gcc" "src/CMakeFiles/pangolin.dir/display/widgets/widgets.cpp.o.d"
+  "/home/cyx/Softwares/SensorsCalibration/lidar2lidar/Pangolin/src/gl/glchar.cpp" "src/CMakeFiles/pangolin.dir/gl/glchar.cpp.o" "gcc" "src/CMakeFiles/pangolin.dir/gl/glchar.cpp.o.d"
+  "/home/cyx/Softwares/SensorsCalibration/lidar2lidar/Pangolin/src/gl/glfont.cpp" "src/CMakeFiles/pangolin.dir/gl/glfont.cpp.o" "gcc" "src/CMakeFiles/pangolin.dir/gl/glfont.cpp.o.d"
+  "/home/cyx/Softwares/SensorsCalibration/lidar2lidar/Pangolin/src/gl/glpangoglu.cpp" "src/CMakeFiles/pangolin.dir/gl/glpangoglu.cpp.o" "gcc" "src/CMakeFiles/pangolin.dir/gl/glpangoglu.cpp.o.d"
+  "/home/cyx/Softwares/SensorsCalibration/lidar2lidar/Pangolin/src/gl/gltext.cpp" "src/CMakeFiles/pangolin.dir/gl/gltext.cpp.o" "gcc" "src/CMakeFiles/pangolin.dir/gl/gltext.cpp.o.d"
+  "/home/cyx/Softwares/SensorsCalibration/lidar2lidar/Pangolin/src/gl/gltexturecache.cpp" "src/CMakeFiles/pangolin.dir/gl/gltexturecache.cpp.o" "gcc" "src/CMakeFiles/pangolin.dir/gl/gltexturecache.cpp.o.d"
+  "/home/cyx/Softwares/SensorsCalibration/lidar2lidar/Pangolin/src/handler/handler.cpp" "src/CMakeFiles/pangolin.dir/handler/handler.cpp.o" "gcc" "src/CMakeFiles/pangolin.dir/handler/handler.cpp.o.d"
+  "/home/cyx/Softwares/SensorsCalibration/lidar2lidar/Pangolin/src/handler/handler_glbuffer.cpp" "src/CMakeFiles/pangolin.dir/handler/handler_glbuffer.cpp.o" "gcc" "src/CMakeFiles/pangolin.dir/handler/handler_glbuffer.cpp.o.d"
+  "/home/cyx/Softwares/SensorsCalibration/lidar2lidar/Pangolin/src/image/image_common.cpp" "src/CMakeFiles/pangolin.dir/image/image_common.cpp.o" "gcc" "src/CMakeFiles/pangolin.dir/image/image_common.cpp.o.d"
+  "/home/cyx/Softwares/SensorsCalibration/lidar2lidar/Pangolin/src/image/image_io.cpp" "src/CMakeFiles/pangolin.dir/image/image_io.cpp.o" "gcc" "src/CMakeFiles/pangolin.dir/image/image_io.cpp.o.d"
+  "/home/cyx/Softwares/SensorsCalibration/lidar2lidar/Pangolin/src/log/packetstream.cpp" "src/CMakeFiles/pangolin.dir/log/packetstream.cpp.o" "gcc" "src/CMakeFiles/pangolin.dir/log/packetstream.cpp.o.d"
+  "/home/cyx/Softwares/SensorsCalibration/lidar2lidar/Pangolin/src/plot/datalog.cpp" "src/CMakeFiles/pangolin.dir/plot/datalog.cpp.o" "gcc" "src/CMakeFiles/pangolin.dir/plot/datalog.cpp.o.d"
+  "/home/cyx/Softwares/SensorsCalibration/lidar2lidar/Pangolin/src/plot/plotter.cpp" "src/CMakeFiles/pangolin.dir/plot/plotter.cpp.o" "gcc" "src/CMakeFiles/pangolin.dir/plot/plotter.cpp.o.d"
+  "/home/cyx/Softwares/SensorsCalibration/lidar2lidar/Pangolin/src/python/PyInterpreter.cpp" "src/CMakeFiles/pangolin.dir/python/PyInterpreter.cpp.o" "gcc" "src/CMakeFiles/pangolin.dir/python/PyInterpreter.cpp.o.d"
+  "/home/cyx/Softwares/SensorsCalibration/lidar2lidar/Pangolin/src/utils/file_extension.cpp" "src/CMakeFiles/pangolin.dir/utils/file_extension.cpp.o" "gcc" "src/CMakeFiles/pangolin.dir/utils/file_extension.cpp.o.d"
+  "/home/cyx/Softwares/SensorsCalibration/lidar2lidar/Pangolin/src/utils/file_utils.cpp" "src/CMakeFiles/pangolin.dir/utils/file_utils.cpp.o" "gcc" "src/CMakeFiles/pangolin.dir/utils/file_utils.cpp.o.d"
+  "/home/cyx/Softwares/SensorsCalibration/lidar2lidar/Pangolin/src/utils/threadedfilebuf.cpp" "src/CMakeFiles/pangolin.dir/utils/threadedfilebuf.cpp.o" "gcc" "src/CMakeFiles/pangolin.dir/utils/threadedfilebuf.cpp.o.d"
+  "/home/cyx/Softwares/SensorsCalibration/lidar2lidar/Pangolin/src/utils/uri.cpp" "src/CMakeFiles/pangolin.dir/utils/uri.cpp.o" "gcc" "src/CMakeFiles/pangolin.dir/utils/uri.cpp.o.d"
+  "/home/cyx/Softwares/SensorsCalibration/lidar2lidar/Pangolin/src/var/input_record_repeat.cpp" "src/CMakeFiles/pangolin.dir/var/input_record_repeat.cpp.o" "gcc" "src/CMakeFiles/pangolin.dir/var/input_record_repeat.cpp.o.d"
+  "/home/cyx/Softwares/SensorsCalibration/lidar2lidar/Pangolin/src/var/vars.cpp" "src/CMakeFiles/pangolin.dir/var/vars.cpp.o" "gcc" "src/CMakeFiles/pangolin.dir/var/vars.cpp.o.d"
+  "/home/cyx/Softwares/SensorsCalibration/lidar2lidar/Pangolin/src/video/drivers/debayer.cpp" "src/CMakeFiles/pangolin.dir/video/drivers/debayer.cpp.o" "gcc" "src/CMakeFiles/pangolin.dir/video/drivers/debayer.cpp.o.d"
+  "/home/cyx/Softwares/SensorsCalibration/lidar2lidar/Pangolin/src/video/drivers/ffmpeg.cpp" "src/CMakeFiles/pangolin.dir/video/drivers/ffmpeg.cpp.o" "gcc" "src/CMakeFiles/pangolin.dir/video/drivers/ffmpeg.cpp.o.d"
+  "/home/cyx/Softwares/SensorsCalibration/lidar2lidar/Pangolin/src/video/drivers/images.cpp" "src/CMakeFiles/pangolin.dir/video/drivers/images.cpp.o" "gcc" "src/CMakeFiles/pangolin.dir/video/drivers/images.cpp.o.d"
+  "/home/cyx/Softwares/SensorsCalibration/lidar2lidar/Pangolin/src/video/drivers/join.cpp" "src/CMakeFiles/pangolin.dir/video/drivers/join.cpp.o" "gcc" "src/CMakeFiles/pangolin.dir/video/drivers/join.cpp.o.d"
+  "/home/cyx/Softwares/SensorsCalibration/lidar2lidar/Pangolin/src/video/drivers/openni.cpp" "src/CMakeFiles/pangolin.dir/video/drivers/openni.cpp.o" "gcc" "src/CMakeFiles/pangolin.dir/video/drivers/openni.cpp.o.d"
+  "/home/cyx/Softwares/SensorsCalibration/lidar2lidar/Pangolin/src/video/drivers/pango_video.cpp" "src/CMakeFiles/pangolin.dir/video/drivers/pango_video.cpp.o" "gcc" "src/CMakeFiles/pangolin.dir/video/drivers/pango_video.cpp.o.d"
+  "/home/cyx/Softwares/SensorsCalibration/lidar2lidar/Pangolin/src/video/drivers/pango_video_output.cpp" "src/CMakeFiles/pangolin.dir/video/drivers/pango_video_output.cpp.o" "gcc" "src/CMakeFiles/pangolin.dir/video/drivers/pango_video_output.cpp.o.d"
+  "/home/cyx/Softwares/SensorsCalibration/lidar2lidar/Pangolin/src/video/drivers/pvn_video.cpp" "src/CMakeFiles/pangolin.dir/video/drivers/pvn_video.cpp.o" "gcc" "src/CMakeFiles/pangolin.dir/video/drivers/pvn_video.cpp.o.d"
+  "/home/cyx/Softwares/SensorsCalibration/lidar2lidar/Pangolin/src/video/drivers/shift.cpp" "src/CMakeFiles/pangolin.dir/video/drivers/shift.cpp.o" "gcc" "src/CMakeFiles/pangolin.dir/video/drivers/shift.cpp.o.d"
+  "/home/cyx/Softwares/SensorsCalibration/lidar2lidar/Pangolin/src/video/drivers/test.cpp" "src/CMakeFiles/pangolin.dir/video/drivers/test.cpp.o" "gcc" "src/CMakeFiles/pangolin.dir/video/drivers/test.cpp.o.d"
+  "/home/cyx/Softwares/SensorsCalibration/lidar2lidar/Pangolin/src/video/drivers/unpack.cpp" "src/CMakeFiles/pangolin.dir/video/drivers/unpack.cpp.o" "gcc" "src/CMakeFiles/pangolin.dir/video/drivers/unpack.cpp.o.d"
+  "/home/cyx/Softwares/SensorsCalibration/lidar2lidar/Pangolin/src/video/drivers/v4l.cpp" "src/CMakeFiles/pangolin.dir/video/drivers/v4l.cpp.o" "gcc" "src/CMakeFiles/pangolin.dir/video/drivers/v4l.cpp.o.d"
+  "/home/cyx/Softwares/SensorsCalibration/lidar2lidar/Pangolin/src/video/drivers/video_splitter.cpp" "src/CMakeFiles/pangolin.dir/video/drivers/video_splitter.cpp.o" "gcc" "src/CMakeFiles/pangolin.dir/video/drivers/video_splitter.cpp.o.d"
+  "/home/cyx/Softwares/SensorsCalibration/lidar2lidar/Pangolin/src/video/video.cpp" "src/CMakeFiles/pangolin.dir/video/video.cpp.o" "gcc" "src/CMakeFiles/pangolin.dir/video/video.cpp.o.d"
+  "/home/cyx/Softwares/SensorsCalibration/lidar2lidar/Pangolin/src/video/video_output.cpp" "src/CMakeFiles/pangolin.dir/video/video_output.cpp.o" "gcc" "src/CMakeFiles/pangolin.dir/video/video_output.cpp.o.d"
+  "/home/cyx/Softwares/SensorsCalibration/lidar2lidar/Pangolin/src/video/video_record_repeat.cpp" "src/CMakeFiles/pangolin.dir/video/video_record_repeat.cpp.o" "gcc" "src/CMakeFiles/pangolin.dir/video/video_record_repeat.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/src/CMakeFiles/pangolin.dir/fonts.c.o.d
@@ -0,0 +1,3 @@
+src/CMakeFiles/pangolin.dir/fonts.c.o: \
+ /home/cyx/Softwares/SensorsCalibration/lidar2lidar/Pangolin/build/src/fonts.c \
+ /usr/include/stdc-predef.h
